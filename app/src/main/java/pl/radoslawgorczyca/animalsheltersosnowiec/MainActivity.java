78f7997ca583b59f4abe8b52fb8b33ae35351afb
Radosław Gorczyca <d33zer@interia.pl>
2017-11-25 18:39:54 +0100

    Działający adapter zwierząt do gridview

--- app/src/main/java/pl/radoslawgorczyca/animalsheltersosnowiec/MainActivity.java
+++ app/src/main/java/pl/radoslawgorczyca/animalsheltersosnowiec/MainActivity.java
@@ -2,12 +2,31 @@ package pl.radoslawgorczyca.animalsheltersosnowiec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.grid_view);
+
+
+        ArrayList<Animal> animals = new ArrayList<>();
+
+        animals.add(new Animal("Reksio", "Rezerwacja", R.mipmap.dog1));
+
+        AnimalAdapter adapter =
+                new AnimalAdapter(this, animals);
+
+        GridView gridView = findViewById(R.id.grid_view);
+
+        gridView.setAdapter(adapter);
+
+
+
     }
 }
